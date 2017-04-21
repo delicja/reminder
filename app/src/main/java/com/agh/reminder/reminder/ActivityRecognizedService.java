1adcd3fe8c19dd06fb3a3ae7087ff55e2352c41b
emilia <emilia.wrobel@gmail.com>
2017-04-21 07:14:32 +0200

    activity running

--- app/src/main/java/com/agh/reminder/reminder/ActivityRecognizedService.java
+++ app/src/main/java/com/agh/reminder/reminder/ActivityRecognizedService.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.agh.reminder.reminder.models.Stopwatch;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
@@ -19,8 +21,12 @@ import java.util.List;
  * helper methods.
  */
 public class ActivityRecognizedService extends IntentService {
+
+    private Stopwatch stopwatch;
+
     public ActivityRecognizedService() {
         super("ActivityRecognizedService");
+        stopwatch = new Stopwatch();
     }
 
     public ActivityRecognizedService(String name) {
@@ -38,36 +44,10 @@ public class ActivityRecognizedService extends IntentService {
     private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
         for (DetectedActivity activity : probableActivities) {
             switch (activity.getType()) {
-                case DetectedActivity.IN_VEHICLE: {
-                    Log.e(ActivityRecognizedService.class.getName(), "In Vehicle: " + activity.getConfidence());
-                    break;
-                }
-                case DetectedActivity.ON_BICYCLE: {
-                    Log.e(ActivityRecognizedService.class.getName(), "On Bicycle: " + activity.getConfidence());
-                    break;
-                }
-                case DetectedActivity.ON_FOOT: {
-                    Log.e(ActivityRecognizedService.class.getName(), "On Foot: " + activity.getConfidence());
-                    break;
-                }
-                case DetectedActivity.RUNNING: {
-                    Log.e(ActivityRecognizedService.class.getName(), "Running: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you running?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                    }
-                    break;
-                }
-                case DetectedActivity.STILL: {
-                    Log.e(ActivityRecognizedService.class.getName(), "Still: " + activity.getConfidence());
-                    break;
-                }
                 case DetectedActivity.WALKING: {
+                    stopwatch.startStopwatch();
                     Log.e(ActivityRecognizedService.class.getName(), "Walking: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
+                    if (activity.getConfidence() >= 35) {
                         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
                         builder.setContentText("Are you walking?");
                         builder.setSmallIcon(R.mipmap.ic_launcher);
@@ -76,10 +56,10 @@ public class ActivityRecognizedService extends IntentService {
                     }
                     break;
                 }
-                case DetectedActivity.UNKNOWN: {
-                    Log.e(ActivityRecognizedService.class.getName(), "Unknown: " + activity.getConfidence());
-                    break;
-                }
+                default:
+                    stopwatch.stopStopwatch();
+                    Toast.makeText(getApplicationContext(), stopwatch.getHumanReadableTime(),
+                            Toast.LENGTH_LONG).show();
             }
         }
     }
