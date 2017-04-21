32e76672cc74a93f49cbfa2ba2de62d0da643bbe
emilia <emilia.wrobel@gmail.com>
2017-04-21 07:57:01 +0200

    activity running

--- app/src/main/java/com/agh/reminder/reminder/MainActivity.java
+++ app/src/main/java/com/agh/reminder/reminder/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -32,8 +31,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent myIntent = new Intent(MainActivity.this, AddActivity.class);
+                MainActivity.this.startActivity(myIntent);
             }
         });
 
