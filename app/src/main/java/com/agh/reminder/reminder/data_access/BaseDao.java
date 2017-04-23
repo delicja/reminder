d0e6ef8e55cf036cc8f9e949175d45c1ddab9526
MarcinDruzgala <marcin.druzgala@solidbrain.com>
2017-04-23 14:56:04 +0200

    save

---
+++ app/src/main/java/com/agh/reminder/reminder/data_access/BaseDao.java
@@ -0,0 +1,13 @@
+package com.agh.reminder.reminder.data_access;
+
+/**
+ * Created by mdruzgala on 23/04/2017.
+ */
+
+interface IBaseDao {
+
+}
+
+
+public class BaseDao implements IBaseDao {
+}
---
+++ app/src/main/java/com/agh/reminder/reminder/data_access/DatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.agh.reminder.reminder.data_access;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.SQLException;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.support.ConnectionSource;
+
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    
+    public DatabaseHelper(Context context, String databaseName, SQLiteDatabase.CursorFactory factory, int databaseVersion) {
+        super(context, databaseName, factory, databaseVersion);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+
+    }
+}
